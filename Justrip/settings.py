84df1c3026344a38c6288addeb80cffbf75a82bb
vignesh-cloud-prog <vigneshun80@gmail.com>
2022-04-15 01:50:05 +0530

    settings updated 3

--- Justrip/settings.py
+++ Justrip/settings.py
@@ -141,9 +141,9 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-]
+STATIC_ROOT = os.path.join(os.path.dirname(
+    BASE_DIR), "static_cdn", "static_root")
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
