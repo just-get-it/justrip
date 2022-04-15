66d12a3f3d4eb388ea755369b5c0ed44f0c9f654
vignesh-cloud-prog <vigneshun80@gmail.com>
2022-04-15 17:23:36 +0530

    static files settings
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

--- Justrip/settings.py
+++ Justrip/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
